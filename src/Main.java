fb8ac1a205495f316f0d2db60010f1445da345f7
bayu <bayuteguhpamuji@gmail.com>
2018-03-07 21:55:59 +0700

    [Bayu] create object avanza and call method print()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Mobil : nama Class
+        // avanza : object
+        /* new Mobil() : constructor
+        * method yang namanya sama dengan nama
+        * fungsi :
+        * - dijalankan pertama kali
+        * - menentukan nilai awal*/
+        Mobil avanza = new Mobil(); //membuat object avanza dengan nilai == Class Mobil
+        avanza.print();
     }
 }
