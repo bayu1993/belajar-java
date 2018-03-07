6bf730d7c25ab4f760fd5185ccfbb10ef51355b6
bayu <bayuteguhpamuji@gmail.com>
2018-03-08 06:26:28 +0700

    [Bayu] modify method addition and substraction

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,16 @@
-public class Calculator {
-    /*declare variable*/
-    int resultAddition;
-    /*create new method addition
+class Calculator {
+    /*create new method addition : for adding operation
     * @return int data type
     * @param int number1 and number2*/
-    public int addition(int number1, int number2){
-        resultAddition = number1 + number2;
-        return resultAddition;
+    int addition(int number1, int number2){
+        return number1 + number2;
     }
+    /* create new method substraction : for substraction operation
+    * @return int data type
+    * @param int number1 and int number 2*/
+    int subtraction(int number1, int number2){
+        return number1 - number2;
+    }
+
+
 }
