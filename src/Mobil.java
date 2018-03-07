292eeefefdafe8be945bc4dacd8ca576424d9900
bayu <bayuteguhpamuji@gmail.com>
2018-03-07 20:47:11 +0700

    [Bayu] tambah desc dan buat variable

--- src/Mobil.java
+++ src/Mobil.java
@@ -1,2 +1,26 @@
 public class Mobil {
+    /*Tipe data
+    * String = text, menggunakan ("")
+    * Integer = number (non desimal)
+    * Long = number (kapasitas)
+    * Float = number menggunakan komah (desimal)
+    * Double = number komah (kapasitas)
+    * Boolean = true / false
+    *
+    * char = karakter
+    * int = number
+    * daouble =
+    * float =
+    * long =
+    * boolean =
+    *
+    * rule kapan kita pake primitif
+    * String untuk class dan int untuk variable (code version)*/
+    Integer umur = 25;
+    int umurSoni = 20;
+
+    Double beratBadan = 60.3;
+    double tinggiBadan = 170.5;
+
+
 }
