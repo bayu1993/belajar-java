d81b83f7d66f1916f08144da1685dbc5c3fb0a93
bayu <bayuteguhpamuji@gmail.com>
2018-03-08 05:56:44 +0700

    [Bayu] declare variable and add method addition

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,16 @@
 public class Calculator {
+    /*declare variable*/
+    int resultAddition;
+    /*create new method addition
+    * @return int
+    * @param int number1 and number2*/
+    public int addition(int number1, int number2){
+        resultAddition = number1 + number2;
+        System.out.println("====== Penjumlahan ======");
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println("=========  +");
+        System.out.println(resultAddition);
+        return resultAddition;
+    }
 }
