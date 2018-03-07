48386fcfc5655239b471530cc7a49471ab4f42cd
bayu <bayuteguhpamuji@gmail.com>
2018-03-08 05:45:15 +0700

    [Bayu] edit desc on src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         // Mobil : nama Class
-        // avanza : object
-        /* new Mobil() : constructor
-        * method yang namanya sama dengan nama
+        // avanza : nama object
+        /* new Mobil() : Constructor : method yang namanya sama dengan nama class
         * fungsi :
         * - menentukan nilai awal
-        * instansiasi object
+        * - instansiasi object
+        * # struktur penulisan
         * NamaClass namaObject = new Constructor
         *
-        * bagusnya?
         * applikasi calculator
         *
-        * //sering dipake dan dinamis
+        * kapan kita inisialisasi variable di Constructor?
+        * sering dipake dan dinamis
         * not static*/
         Mobil avanza = new Mobil(); //membuat object avanza dengan nilai == Class Mobil
         avanza.print();
