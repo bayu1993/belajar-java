bf7ce21d7bf313a35fa4493e1346e2b52feb803f
bayu <bayuteguhpamuji@gmail.com>
2018-03-07 21:04:40 +0700

    [Bayu] tambah variable class mobil

--- src/Mobil.java
+++ src/Mobil.java
@@ -15,12 +15,21 @@ public class Mobil {
     * boolean =
     *
     * rule kapan kita pake primitif
-    * String untuk class dan int untuk variable (code version)*/
-    Integer umur = 25;
+    * String (yg pake huruf besar)untuk class dan int untuk variable (code version)*/
+    /*Integer umur = 25;
     int umurSoni = 20;
 
     Double beratBadan = 60.3;
-    double tinggiBadan = 170.5;
+    double tinggiBadan = 170.5;*/
+
+    int jumlahRoda = 4;
+    boolean mesinDepan = true;
+    String warna = "merah";
+    String setir = "Bulat";
+    boolean pintuBukaAtas = true;
+    int jmlPintu = 4;
+    int jmlKnalpot = 2;
+    int jmlTempatDuduk = 4;
 
 
 }
