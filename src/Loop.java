68266dc242dbea6efca89377c333e1311a152624
bayu <bayuteguhpamuji@gmail.com>
2018-03-09 16:43:14 +0700

    [Bayu] add Class Condition or if else

--- src/Loop.java
+++ src/Loop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Loop {
     public static void main(String[] args) {
         int [] numbers ={12,23,4323,123,1,1543};
@@ -9,6 +11,26 @@ public class Loop {
         for (String newName :names){
             System.out.print(newName + " ");
 
+        }
+    }
+
+}
+class Condition{
+    public static void main(String[] args) {
+        Scanner newInput = new Scanner(System.in);
+
+        System.out.print("ketikkan nomor pertama :");
+        int firstNumber = newInput.nextInt();
+
+        System.out.print("ketikkan nomor kedua :");
+        int secondNumber = newInput.nextInt();
+
+        if (firstNumber > secondNumber){
+            System.out.println("nomor pertama lebih besar");
+        }else if (firstNumber == secondNumber){
+            System.out.println("nomor pertama == nomor kedua");
+        }else {
+            System.out.println("nomor kedua lebih besar rupanya");
         }
     }
 
