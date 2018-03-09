a1f172c6dccdaf751f300d1b249d4e91bb03455e
bayu <bayuteguhpamuji@gmail.com>
2018-03-09 17:10:05 +0700

    [Bayu] coba mengkombinasikan if else switch

--- src/Loop.java
+++ src/Loop.java
@@ -17,6 +17,7 @@ public class Loop {
 }
 class Condition{
     public static void main(String[] args) {
+        Condition newObject = new Condition();
         Scanner newInput = new Scanner(System.in);
 
         System.out.print("ketikkan nomor pertama :");
@@ -25,9 +26,34 @@ class Condition{
         System.out.print("ketikkan nomor kedua :");
         int secondNumber = newInput.nextInt();
 
-        if (firstNumber > secondNumber){
+        newObject.minMax(firstNumber,secondNumber);
+
+        System.out.println("Apakah anda ingin coba lagi?(y or n)");
+        String keputusan = newInput.next();
+        switch (keputusan){
+            case "y":
+                System.out.println("kamu pilih yes");
+                System.out.print("ketikkan nomor pertama :");
+                firstNumber = newInput.nextInt();
+
+                System.out.print("ketikkan nomor kedua :");
+                secondNumber = newInput.nextInt();
+
+                newObject.minMax(firstNumber,secondNumber);
+                break;
+            case "n":
+                System.out.println("kamu pilih no");
+                System.out.println("thanks for try this simple program");
+                break;
+            default:
+                System.out.println("please enter y or n");
+        }
+
+    }
+    public void minMax(int num1, int num2){
+        if (num1 > num2){
             System.out.println("nomor pertama lebih besar");
-        }else if (firstNumber == secondNumber){
+        }else if (num1 == num2){
             System.out.println("nomor pertama == nomor kedua");
         }else {
             System.out.println("nomor kedua lebih besar rupanya");
