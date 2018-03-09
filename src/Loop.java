f834d19cb3a109ac6d66bb5ff38fab98142451bd
bayu <bayuteguhpamuji@gmail.com>
2018-03-09 14:53:56 +0700

    [Bayu] showing array with for loop

--- src/Loop.java
+++ src/Loop.java
@@ -1,2 +1,15 @@
 public class Loop {
+    public static void main(String[] args) {
+        int [] numbers ={12,23,4323,123,1,1543};
+        String [] names = {"alex","nero","beri","agung","chris","Elon","Steve"};
+        for (int a : numbers){
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (String newName :names){
+            System.out.print(newName + " ");
+
+        }
+    }
+
 }
